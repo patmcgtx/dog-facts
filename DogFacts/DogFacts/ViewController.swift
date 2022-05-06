d1569fcaf0e6f021de251338e6aefd01403ccc69
Patrick <pmcgonigle@gmail.com>
2022-05-05 22:13:04 -0500

    Initial empty Mac Xcode proect

---
+++ DogFacts/DogFacts/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DogFacts
+//
+//  Created by Patrick McGonigle on 5/5/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
