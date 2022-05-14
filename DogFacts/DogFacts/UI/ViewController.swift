d1e11f411b9eee6ec8cb6d09357d3963578b2036
Patrick <pmcgonigle@gmail.com>
2022-05-14 18:12:48 -0500

    Cleanup

--- DogFacts/DogFacts/UI/ViewController.swift
+++ DogFacts/DogFacts/UI/ViewController.swift
@@ -65,11 +65,4 @@ class ViewController: NSViewController {
         }
     }
     
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
