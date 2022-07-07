e5cd40e6a37e78417767496ccdb200dcb502bd21
Patrick <pmcgonigle@gmail.com>
2022-07-07 18:42:04 -0500

    Added basic SwiftUI target

---
+++ DogFacts/Shared/DogFactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFactsSwiftUIApp.swift
+//  Shared
+//
+//  Created by Patrick McGonigle on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DogFactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
