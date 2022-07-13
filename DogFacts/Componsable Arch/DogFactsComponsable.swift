0a56802c3034417d0f41eacf94cb2bd649b29188
Patrick <pmcgonigle@gmail.com>
2022-07-12 22:49:06 -0500

    Added initial, untested TCA types

---
+++ DogFacts/Componsable Arch/DogFactsComponsable.swift
@@ -0,0 +1,30 @@
+//
+//  DogFactsComponsable.swift
+//  DogFacts
+//
+//  Created by Patrick McGonigle on 7/12/22.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct DogFactsState: Equatable {
+    var docFact: String?
+    var isLoading: Bool = false
+}
+
+enum DogFactsAction: Equatable {
+    case fetch
+    case fetchResponse(Result<String, DogFactsError>)
+}
+
+struct DogFactsEnvironment {
+    var service: DogFactsService
+}
+
+let refreshableReducer = Reducer<
+    DogFactsState,
+    DogFactsAction,
+    DogFactsEnvironment> { state, action, environment in
+    return .none
+}
